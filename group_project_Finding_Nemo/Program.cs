8f0e994ba85eb299aebd4b6552ae650317bd2194
User <User@DESKTOP-85CMJ2H>
2023-12-13 22:45:28 +0800

    Add project files.

---
+++ group_project_Finding_Nemo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+        Navigate(new HomePage());
+    }
+
+    private void Navigate(Page page)
+    {
+        FrameContent.NavigationService.Navigate(page);
+    }
+
+    private void HomeButton_Click(object sender, RoutedEventArgs e)
+    {
+        Navigate(new HomePage());
+    }
+
+    private void EntryButton_Click(object sender, RoutedEventArgs e)
+    {
+        Navigate(new EntryPage());
+    }
+
+    private void AboutButton_Click(object sender, RoutedEventArgs e)
+    {
+        Navigate(new AboutPage());
+    }
+}
